914c5be1f0bb41f7e2fab86519241b9bc8ecc81a
Rajani <rajani.kami004@gmail.com>
2026-05-04 10:21:36 -0400

    start lab 15

---
+++ Lab15_rbaraili1.py
@@ -0,0 +1,10 @@
+"""The program's name:  Plot a Math Formula
+Your name (the author): Rajani Baraili
+The purpose of the program: uses matplotlib to plot one of your favorite math formulas
+Any info about starter code (If used, where it came from, link, etc.), and the: none
+Date: 5/4/2026"""
+
+x = []
+y= []
+
+
