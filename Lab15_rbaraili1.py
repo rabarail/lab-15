15c2ce1883cb5690e500daa51072851a32ea63ec
Rajani <rajani.kami004@gmail.com>
2026-05-05 16:29:54 -0400

    defining variable

--- Lab15_rbaraili1.py
+++ Lab15_rbaraili1.py
@@ -3,7 +3,14 @@ Your name (the author): Rajani Baraili
 The purpose of the program: uses matplotlib to plot one of your favorite math formulas
 Any info about starter code (If used, where it came from, link, etc.), and the: none
 Date: 5/4/2026"""
+import math
+import matplotlib.pyplot as plt
 
+x = []
+y = []
 
-
+t = 0.0 # where angle starts
+step = 0.05 
+a = 1
+t = 0.0
 
